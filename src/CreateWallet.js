591d579aabdd731aa5c76e61e2642586f6442861
royvardhan <royvardhan.official@gmail.com>
2022-06-21 19:36:07 +0530

    .

--- src/CreateWallet.js
+++ src/CreateWallet.js
@@ -1,7 +1,7 @@
 export default function WelcomePage() {
     return (
         <page className="flex justify-center p-20 ">
-        <div className="w-80 bg-black p-4 rounded-3xl h-12pro sm:w-96 ">
+        <div className="w-80 bg-black p-4 rounded-3xl h-12pro sm:w-screen">
             <div className="flex place-content-end text-sm">
             <button className="">Skip</button>
             </div>
