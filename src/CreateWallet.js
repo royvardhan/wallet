49bbf807e94a48adca46c82479386f7135b4f351
royvardhan <royvardhan.official@gmail.com>
2022-06-21 20:02:03 +0530

    .

--- src/CreateWallet.js
+++ src/CreateWallet.js
@@ -1,13 +1,14 @@
+import One from "./utils/1.jpg"
 export default function WelcomePage() {
     return (
-        <page className="flex justify-center m-20 sm:p-10 sm:m-2 ">
-        <div className="w-80 bg-black p-4 rounded-3xl h-12pro sm:w-screen">
+        <page className="flex justify-center m-20 sm:p-2 sm:m-2 ">
+        <div className="w-80 bg-black p-4 rounded-3xl h-12pro sm:w-screen" >
             <div className="flex place-content-end text-sm">
-            <button className="">Skip</button>
+            <button className="mt-5">Skip</button>
             </div>
-            <h1 className="text-3xl">Set up your wallet</h1>
-            <p className="w-64 mb-5">Import your wallet using seed phrase or set up a new one </p>
-            <container className="text-sm flex">
+            <h1 className="text-3xl mt-20">Set up your wallet</h1>
+            <p className="w-64 mb-5 mt-2">Import your wallet using seed phrase or set up a new one </p>
+            <container className="text-sm flex mt-20">
                 <box1 className=" p-10 rounded-xl bg-yellow-100">
                     <h2 >Import your wallet</h2>
                 </box1>
